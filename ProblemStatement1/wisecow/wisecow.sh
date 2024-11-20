1408e70cd8fddf28e84483c07e535865780d2266
tanshaella <shaella.tan@gmail.com>
2024-11-20 19:05:40 +0700

    Change -lN to -ln on line 41

--- ProblemStatement1/wisecow/wisecow.sh
+++ ProblemStatement1/wisecow/wisecow.sh
@@ -38,7 +38,7 @@ main() {
 	echo "Wisdom served on port=$SRVPORT..."
 
 	while [ 1 ]; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+		cat $RSPFILE | nc -ln $SRVPORT | handleRequest
 		sleep 0.01
 	done
 }
